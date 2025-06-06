ccadf08e830cb3fe5e926c8495e27d3548ab2258
Kim Spiering <kim.spiering@gmx.ch>
2025-06-06 14:48:15 +0200

    project mpl ag hinzugefügt

--- pages/projects.php
+++ pages/projects.php
@@ -4,14 +4,39 @@ $title = "Projects";
 <?php include "../attributes/header.php"; ?>
 <section class="projects__main">
     <h1>Projects</h1>
+    <p>Here you'll find a selection of my past work, including web apps, creative concepts, and business websites I've developed during my studies, work life and personal explorations.</p>
 </section>
 
-<section class="projects__videos">
-    <div class="mein-video">
-        <video controls autoplay muted loop>
-            <source src="/assets/videos/Abschluss-Projekt-2025.mov" type="video/mp4">
-            Dein Browser unterstützt das Video-Tag nicht.
-        </video>
+<section class="projects__main">
+    <h2>Final Project – SAE Institute Zurich (2025)</h2>
+    <div class="projects__videos">
+        <div class="homepage__content">
+            <video controls autoplay muted loop height="auto" width="380px">
+                <source src="/assets/videos/abschluss-projekt-2025.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+        </div>
+        <div class="homepage__content2">
+            <p>
+                This project was developed as part of my graduation at SAE Zurich.
+                The concept was to design and build a modern, user-friendly web application.
+                The app features dynamic content presentation, clean design aesthetics, and a smooth user experience that reflects
+                the atmosphere of a real-world gallery space. The goal was to blend technology and creativity, offering users an
+                immersive way to explore art online.</p>
+        </div>
+    </div>
+</section>
+
+<section class="projects__main">
+    <h2>Redesign – MPL AG (2025)</h2>
+    <h3>Before & After</h3>
+    <div class="projects__videos">
+        <div class="homepage__content">
+            <a href="www.mpl.ch"><img class="projects__img" src="/assets/images/homepage-mpl-ag.jpeg" alt="Homepage MPL AG"></a>
+        </div>
+        <div class="homepage__content2">
+            <a href="www.mpl.ch"><img class="projects__img" src="/assets/images/homepage-mpl-ag-redesign.jpeg" alt="Homepage MPL AG"></a>
+        </div>
     </div>
 </section>
 <?php include "../attributes/footer.php"; ?>
