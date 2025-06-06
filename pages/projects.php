8d688ca15eba6f9c6dbc486a752878e5f96b8264
Kim Spiering <kim.spiering@gmx.ch>
2025-06-06 13:52:00 +0200

    video

--- pages/projects.php
+++ pages/projects.php
@@ -6,4 +6,12 @@ $title = "Projects";
     <h1>Projects</h1>
 </section>
 
+<section class="projects__videos">
+    <div class="mein-video">
+        <video controls autoplay muted loop>
+            <source src="/assets/videos/Abschluss-Projekt-2025.mov" type="video/mp4">
+            Dein Browser unterstützt das Video-Tag nicht.
+        </video>
+    </div>
+</section>
 <?php include "../attributes/footer.php"; ?>
